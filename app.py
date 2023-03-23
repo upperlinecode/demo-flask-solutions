c8263733b42a23e592f210088f60467e9b55a5f8
Manny Rodriguez <66538419+er-arcadio@users.noreply.github.com>
2023-03-23 19:21:28 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 # ---- YOUR APP STARTS HERE ----
+
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request, redirect
+
+from model import get_breakfast_rating
+from datetime import datetime
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
 # -- Routes section --
+
+# INDEX
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+  props = {
+    "title": "My Breakfast Shouter",
+    "first_name":"Steven"
+  }
+  return render_template("index.html", time = datetime.now(), props=props)
+
+# RESULTS
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+  if request.method == 'POST':
+    print(request.form)
+    user_name = request.form["nickname"]
+    user_breakfast = request.form["breakfast"]
+    breakfast_rating = get_breakfast_rating(user_breakfast)
+    return render_template(
+      "breakfast.html", 
+      user_name=user_name,
+      user_breakfast=user_breakfast,
+      breakfast_rating=breakfast_rating
+    )
+  else:
+    return redirect('/')
+  
+# SECRET
+@app.route('/secret')
+def secret():
+  return "<h1>You found the secret!</h1>"
---
+++ model.py
@@ -0,0 +1,5 @@
+def get_breakfast_rating(food):
+  if food.lower() == "waffles":
+    return "That's an AMAZING breakfast!"
+  else:
+    return "That's a good breakfast."
